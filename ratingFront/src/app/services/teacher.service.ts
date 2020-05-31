1089cde416f4d92430935b885c1f277abb259a2b
kjerinic <katarinajerinic8@gmail.com>
2020-05-31 14:02:44 +0200

    Add headers to API calls in Teacher service

--- ratingFront/src/app/services/teacher.service.ts
+++ ratingFront/src/app/services/teacher.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Teacher} from '../models/Teacher';
 
@@ -16,28 +16,31 @@ export class TeacherService {
     return url + fnName;
   }
 
-  addNewTeacher(teacher: Teacher): Observable<any> {
-    return this.httpClient.post<any>(this.getAPIUrl('teachers'),
-      {
-        fullName: teacher.fullName,
-        title: teacher.title
-      });
-  }
-
   getAllTeachers(): Observable<Teacher[]> {
     return this.httpClient.get<Teacher[]>(this.getAPIUrl('teachers'));
   }
 
+  addNewTeacher(teacher: Teacher): Observable<any> {
+    const params = new HttpHeaders().set('username', 'admin');
+    return this.httpClient.post<any>(this.getAPIUrl('teachers'),
+      {
+        fullName: teacher.fullName,
+        title: teacher.title
+      }, {headers: params});
+  }
+
   editTeacher(teacher: Teacher): Observable<any> {
+    const params = new HttpHeaders().set('username', 'admin');
     return this.httpClient.put<any>(this.getAPIUrl('teachers'),
       {
         id: teacher.id,
         fullName: teacher.fullName,
         title: teacher.title
-      });
+      }, {headers: params});
   }
 
   deleteTeacher(teacher: Teacher): Observable<any> {
-    return this.httpClient.delete<any>(this.getAPIUrl('teachers/' + teacher.id));
+    const params = new HttpHeaders().set('username', 'admin');
+    return this.httpClient.delete<any>(this.getAPIUrl('teachers/' + teacher.id), {headers: params});
   }
 }
